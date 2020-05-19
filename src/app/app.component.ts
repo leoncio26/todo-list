d82d301fe53d4814e436a7170483295de09038c7
unknown <leonciosantana@globaltecnologia.net>
2020-05-18 21:27:09 -0300

    Botão de novo em projetos adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IndexedDBApiService } from './indexedDBApi.service';
+import { Project } from './project';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,26 @@ import { IndexedDBApiService } from './indexedDBApi.service';
 })
 export class AppComponent implements OnInit{
   title = 'todo';
+  projects: Array<Project> = [];
 
   constructor(private indexedDBApiService: IndexedDBApiService){}
 
   ngOnInit():void {
-    this.indexedDBApiService.initialize();
+    //this.indexedDBApiService.initialize();
+    const p1 = <Project>{
+      name: 'Primeiro projeto',
+      date: new Date()
+    }
+    const p2: Project = {
+      name: 'Segundo projeto',
+      date: new Date()
+    }
+
+    this.projects.push(p1);
+    this.projects.push(p2);
+  }
+
+  newProject(){
+    alert('Novo projeto');
   }
 }
